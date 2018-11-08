fe58e46fee5eb5b151b802edfdd3e3640caa1741
nash-kumar <nareshkumar.bn@accionlabs.com>
2018-11-08 12:27:40 +0530

    changes

--- router/router.js
+++ router/router.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const route = express.Router();
-module.exports = route
+const router = express.Router();
+module.exports = router
+const userModel=require('../model/model').userModel;
+
 
 router.post('/register', (req, res) => {
     console.log('POST IS WORKING!');
@@ -50,4 +52,29 @@ router.post('/login', (req, res) => {
 
 
     });
+});
+
+//Card API
+router.post('/card', (res,req) => {
+    if (req.body.data) {
+        const user = userModel({
+             creditCard: req.body.data.creditCard,
+             debitCard: req.body.data.debitCard,
+        });
+        user.save((err, result) => {
+            if (err) {
+                res.status(500).send({
+                    success: false,
+                    message: err.message
+                });
+            } else if (result) {
+                res.status(201).send({ success: true, message: "Data added successfully", result });
+            }
+        });
+    } else {
+        res.status(400).json({
+            message: 'Please Enter any DATA!'
+        });
+        
+    }
 });
