b5548a47f3323def38ce5527c70b4f9bbc769094
nash-kumar <nareshkumar.bn@accionlabs.com>
2018-11-09 12:42:58 +0530

    login-reg

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const express = require('express');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
 require('dotenv').config();
-const app = express();
 
-const mongoose = require('mongoose');
+// Init App
+var app = express();
+
+const cors = require('cors');
+app.use(cors());
+
 
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true}, (err)=>{
     if(err) console.log('Could not Connect');
@@ -10,12 +25,56 @@ mongoose.connect(process.env.DB_URL, {useNewUrlParser: true}, (err)=>{
 })
 
 
-const cors = require('cors');
-app.use(cors());
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+  
+      while(namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param : formParam,
+        msg   : msg,
+        value : value
+      };
+    }
+  }));
+  
+  // Connect Flash
+  app.use(flash());
+  
+  // Global Vars
+  app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+  });
+  
 
 const user = require('./router/router');
 app.use('/user', user);
-
 
 app.get('/', (req, res) => res.send("Hello Page"));
 
--- router/router.js
+++ router/router.js
@@ -1,80 +1,195 @@
 const express = require('express');
 const router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 module.exports = router
-const userModel=require('../model/model').userModel;
+const User =require('../model/model');
 
 
-router.post('/register', (req, res) => {
-    console.log('POST IS WORKING!');
-    if (req.body.data) {
-        const user = userModel({
-            firstname: req.body.firstname,
-            lastname: req.body.surname,
-            email: req.body.email,
-            password: req.body.password
-        });
-        user.save((err, result) => {
-            if (err) {
-                res.status(500).send({
-                    success: false,
-                    message: err.message
-                });
-            } else if (result) {
-                res.status(201).send({ success: true, message: "Data added successfully", result });
-            }
-        });
-    } else {
-        res.status(400).json({
-            message: 'Please Enter any DATA!'
-        });
-    }
+// router.post('/register', (req, res) => {
+//     console.log('POST IS WORKING!');
+//     if (req.body.data) {
+//         const user = userModel({
+//             firstname: req.body.data.firstname,
+//             lastname: req.body.data.lastname,
+//             email: req.body.data.email,
+//             password: req.body.data.password
+//         });
+//         user.save((err, result) => {
+//             if (err) {
+//                 res.status(500).send({
+//                     success: false,
+//                     message: err.message
+//                 });
+//             } else if (result) {
+//                 res.status(201).send({ success: true, message: "Data added successfully", result });
+//             }
+//         });
+//     } else {
+//         res.status(400).json({
+//             message: 'Please Enter any DATA!'
+//         });
+//     }
+// });
+router.get('/', ensureAuthenticated, function(req, res){
+	console.log('index');
 });
 
-router.post('/login', (req, res) => {
-    const email = req.body.email;
-    var password = req.body.password;
+//Authenticate API
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		req.flash('error_msg','You are not logged in');
+		// res.redirect('/users/login');
+	}   
+}
 
-    userModel.findOne({ email: req.body.user.email }, function (err, userInfo) {
+//Register API
+router.post('/register', function (req, res) {
+    var firstname = req.body.firstname;
+    var lastname = req.body.lastname;   
+	var email = req.body.email;
+	var password = req.body.password;
+	var password2 = req.body.password2;
 
-        if (err) {
-            next(err);
-        } if (userInfo) {
-            if (bcrypt.compareSync(req.body.data.user.password, userInfo.password)) {
-                const token = jwt.sign({ id: userInfo._id }, req.app.get('secretKey'), { expiresIn: '1h' });
-                res.json({ success: true, message: "user found!!!", data: { user: userInfo, token: token } });
-            } else {
-                res.json({ success: false, message: "Invalid email/password!!!" });
-            }
-        }
-        if (!userInfo) {
-            res.json({ success: false, message: "Invalid email/password!!!" });
-        }
+	// Validation
+	req.checkBody('firstname', 'Name is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
 
+	var errors = req.validationErrors();
 
-    });
+	if (errors) {
+		res.render('register', {
+			errors: errors
+		});
+	}
+	else {
+		//checking for email are already taken
+		User.findOne({ email: { 
+			"$regex": "^" + email + "\\b", "$options": "i"
+	}}, function (err, user) {
+			User.findOne({ email: { 
+				"$regex": "^" + email + "\\b", "$options": "i"
+		}}, function (err, mail) {
+				if (user || mail) {
+					res.render('register', {
+						user: user,
+						mail: mail
+					});
+				}
+				else {
+					var newUser = new User({
+						firstname: firstname,
+						lastname: lastname,
+						email: email,
+						password: password
+					});
+					User.createUser(newUser, function (err, user) {
+						if (err) throw err;
+						res.status(200).send(user);
+					});
+         	req.flash('success_msg', 'You are registered and can now login');
+					//res.redirect('/users/login');
+				}
+			});
+		});
+	}
 });
 
-//Card API
-router.post('/card', (res,req) => {
-    if (req.body.data) {
-        const user = userModel({
-             creditCard: req.body.data.creditCard,
-             debitCard: req.body.data.debitCard,
-        });
-        user.save((err, result) => {
-            if (err) {
-                res.status(500).send({
-                    success: false,
-                    message: err.message
-                });
-            } else if (result) {
-                res.status(201).send({ success: true, message: "Data added successfully", result });
-            }
-        });
-    } else {
-        res.status(400).json({
-            message: 'Please Enter any DATA!'
-        });
+passport.use(new LocalStrategy(
+	function (email, password, done) {
+		User.getUserByUsername(email, function (err, user) {
+			if (err) throw err;
+			if (!user) {
+				return done(null, false, { message: 'Unknown User' });
+			}
+
+			User.comparePassword(password, user.password, function (err, isMatch) {
+				if (err) throw err;
+				if (isMatch) {
+					return done(null, user);
+				} else {
+					return done(null, false, { message: 'Invalid password' });
+				}
+			});
+		});
+	}));
+
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+	User.getUserById(id, function (err, user) {
+		done(err, user);
+	});
+});
+
+router.post('/login',
+	passport.authenticate('local', { successRedirect: '/', failureRedirect: '/users/login', failureFlash: true }),
+	function (req, res) {
+		res.redirect('/');
+	});
+
+router.get('/logout', function (req, res) {
+	req.logout();
+
+	req.flash('success_msg', 'You are logged out');
+
+	res.redirect('/users/login');
+});
+
+
+// router.post('/login', (req, res) => {
+//     const email = req.body.email;
+//     var password = req.body.password;
+
+//     userModel.findOne({ email: req.body.user.email }, function (err, userInfo) {
+
+//         if (err) {
+//             next(err);
+//         } if (userInfo) {
+//             if (bcrypt.compareSync(req.body.data.user.password, userInfo.password)) {
+//                 const token = jwt.sign({ id: userInfo._id }, req.app.get('secretKey'), { expiresIn: '1h' });
+//                 res.json({ success: true, message: "user found!!!", data: { user: userInfo, token: token } });
+//             } else {
+//                 res.json({ success: false, message: "Invalid email/password!!!" });
+//             }
+//         }
+//         if (!userInfo) {
+//             res.json({ success: false, message: "Invalid email/password!!!" });
+//         }
+
+
+//     });
+// });
+
+// //Card API
+// router.post('/card', (res,req) => {
+//     if (req.body.data) {
+//         const user = userModel({
+//              creditCard: req.body.data.creditCard,
+//              debitCard: req.body.data.debitCard,
+//         });
+//         user.save((err, result) => {
+//             if (err) {
+//                 res.status(500).send({
+//                     success: false,
+//                     message: err.message
+//                 });
+//             } else if (result) {
+//                 res.status(201).send({ success: true, message: "Data added successfully", result });
+//             }
+//         });
+//     } else {
+//         res.status(400).json({
+//             message: 'Please Enter any DATA!'
+//         });
         
-    }
-});
+//     }
+// });
